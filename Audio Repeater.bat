87f9a4d45b527f0612ef19e545deb122bcdd28d8
Eric Albrecht <eric.albrecht@hotmail.ca>
2021-11-18 06:48:10 -0700

    Update Audio Repeater.bat
    Still playing around with resync, prefill, bufferparts and bufferms in my struggle to prevent the audio streams from becoming distorted.

--- Audio Repeater.bat
+++ Audio Repeater.bat
@@ -22,28 +22,28 @@ set autostart=/AutoStart
 
 set wn[0]=Audio Repeater Line 1 System
 set input[0]=Line 1 (Virtual Audio Cable)
-set resync[0]=20
-set prefill[0]=50
-set bufferparts[0]=15
-set bufferms[0]=150
+set resync[0]=25
+set prefill[0]=70
+set bufferparts[0]=10
+set bufferms[0]=200
 set bps[0]=24
 set sr[0]=96000
 
 set wn[1]=Audio Repeater Line 2 Game
 set input[1]=Line 2 (Virtual Audio Cable)
-set resync[1]=20
-set prefill[1]=50
-set bufferparts[1]=15
-set bufferms[1]=150
+set resync[1]=25
+set prefill[1]=70
+set bufferparts[1]=10
+set bufferms[1]=200
 set bps[1]=24
 set sr[1]=96000
 
 set wn[2]=Audio Repeater Line 3 Voice Chat
 set input[2]=Line 3 (Virtual Audio Cable)
-set resync[2]=20
-set prefill[2]=50
-set bufferparts[2]=15
-set bufferms[2]=150
+set resync[2]=25
+set prefill[2]=70
+set bufferparts[2]=10
+set bufferms[2]=200
 set bps[2]=16
 set sr[2]=48000
 
