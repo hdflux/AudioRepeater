b9095bc756eda222b51fd6d57e51a05ce2ad6ce1
Eric Albrecht <eric.albrecht@hotmail.ca>
2022-04-22 12:05:46 -0600

    Update Audio Repeater.bat
    Trying to remove glitches in audio stream without introducing crackling in the buffer due to sync issues.

--- Audio Repeater.bat
+++ Audio Repeater.bat
@@ -13,6 +13,9 @@ REM wn[#]			/WindowName:<str>		Name of application instance window
 REM 				/AutoStart				Start audio transfer automatically <blank | autostart>
 REM 				/CloseInstance:<str>	Close a specified Audio Repeater instance by its window name
 
+REM Smaller bufferms allows larger bufferparts.
+REM More bufferparts mean more accuracy in buffer but can create more overhead.
+
 REM srcdir	-- Location of audiorepeater.exe.
 set srcdir=H:\Apps\Virtual Audio Cable
 set priority=normal
@@ -22,30 +25,30 @@ set autostart=/AutoStart
 
 set wn[0]=Audio Repeater Line 1 System
 set input[0]=Line 1 (Virtual Audio Cable)
-set resync[0]=25
-set prefill[0]=70
-set bufferparts[0]=10
-set bufferms[0]=200
+set resync[0]=35
+set prefill[0]=50
+set bufferparts[0]=15
+set bufferms[0]=150
 set bps[0]=24
 set sr[0]=96000
 
 set wn[1]=Audio Repeater Line 2 Game
 set input[1]=Line 2 (Virtual Audio Cable)
-set resync[1]=25
-set prefill[1]=70
-set bufferparts[1]=10
-set bufferms[1]=200
+set resync[1]=35
+set prefill[1]=50
+set bufferparts[1]=15
+set bufferms[1]=150
 set bps[1]=24
 set sr[1]=96000
 
 set wn[2]=Audio Repeater Line 3 Voice Chat
 set input[2]=Line 3 (Virtual Audio Cable)
-set resync[2]=25
-set prefill[2]=70
-set bufferparts[2]=10
-set bufferms[2]=200
+set resync[2]=35
+set prefill[2]=50
+set bufferparts[2]=15
+set bufferms[2]=150
 set bps[2]=16
-set sr[2]=48000
+set sr[2]=96000
 
 
 echo Set Audio Repeater to which device?
@@ -71,7 +74,7 @@ goto :gc
 
 :headphones
 
-set out=Line (MG-XU)
+set out=Line (2- MG-XU)
 echo.
 echo Repeating audio to headphones on device %out%...
 goto :gc
