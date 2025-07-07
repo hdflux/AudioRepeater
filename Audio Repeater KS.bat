2c6e002d1dea3ef13cf614299b2fdbfd845cfbba
Eric Albrecht <eric.albrecht@hotmail.ca>
2025-07-07 14:42:28 -0600

    Version 2.2.6
    - Unofficial bug fix release of audiorepeater_ks.exe which allows endpoint numbers to be used in the /Output parameter.
    - Removed workaround code for /Output.

--- Audio Repeater KS.bat
+++ Audio Repeater KS.bat
@@ -13,57 +13,43 @@ echo 3. Close all instances of Audio Repeater.
 echo 4. Quit.
 choice /t 15 /c 1234 /n /d 1
 
-if errorlevel 4 goto :end
-if errorlevel 3 goto :gc
-if errorlevel 2 goto :mixer
-if errorlevel 1 goto :speakers
+if errorlevel 4 goto :EndOfApp
+if errorlevel 3 goto :GarbageCollection
+if errorlevel 2 goto :Mixer
+if errorlevel 1 goto :Speakers
 
 
-:speakers
+:Speakers
 
-set "playbackDir=Speakers"
-:: Uncomment the following two lines to prevent Line 3 Audio Repeater for auto starting and starting minimized.
-:: Useful if you wish to change the Wave Out device in the case where the desired device name is shown more than once and is not listed first.
-::set "vac3_Min="
-::set "vac3_AutoStart="
-
-echo.
 echo [96mConfiguring audio repeaters to speakers.[0m
+set "playbackDir=Speakers"
+goto :GarbageCollection
 
-goto :gc
 
+:Mixer
 
-:mixer
-
-set "playbackDir=Mixer"
-:: Uncomment the following two lines to prevent Line 3 Audio Repeater for auto starting and starting minimized.
-:: Useful if you wish to change the Wave Out device in the case where the desired device name is shown more than once and is not listed first.
-::set "vac3_Min="
-::set "vac3_AutoStart="
-
-echo.
 echo [96mConfiguring audio repeaters to Yamaha MG10XU.[0m
+set "playbackDir=Mixer"
+goto :GarbageCollection
 
+:GarbageCollection
 
-:gc
-
-echo.
 echo [96mScanning for and closing existing Audio Repeater instances...[0m
-echo.
 
 :: Loop through known Audio Repeater instances
 set /a lastIndex=vacCount - 1
 for /L %%i in (0,1,!lastIndex!) do (
-	taskkill /fi "WINDOWTITLE eq !vac%%i_WindowName!" /f >nul 2>&1
+	::audiorepeater_ks.exe /CloseInstance:"!vac%%i_WindowName!"	:: Will work in a future release of audiorepeater_ks.exe.
+	taskkill /FI "WINDOWTITLE eq !vac%%i_WindowName!" /F >nul 2>&1
 )
 
 :: Wait if any were closed
 timeout /t %interval% /nobreak >nul
 
-:: Goto End if no audio repeaters are flagged for creation.
+:: Goto EndOfApp if no audio repeaters are flagged for creation.
 if not defined playbackDir (
     echo [91mNo device selected. Skipping instance creation.[0m
-    goto :end
+    goto :EndOfApp
 )
 
 goto :createinstances
@@ -84,9 +70,7 @@ for /L %%i in (0,1,!lastIndex!) do (
 )
 
 
+:EndOfApp
 
-:end
-
-echo.
 echo [92mCOMPLETE.[0m
 timeout /t 15
